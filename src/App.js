70c94a4dcab7e7c0417f398fd2304ce5a9f46c0a
Sergio Esteban Torres <myplancash@gmail.com>
2023-05-03 12:08:15 -0500

    incorporating a Routing system to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import Categories from './components/Categories/Categories.jsx';
+import Home from './routes/home/Home';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+
+function Shop() {
+  return (
+    <h1>Hello from show component</h1>
+  )
+}
 
 function App() {
-
-  const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
-
   return (
-    <Categories categories={categories}/>
+    <Routes>
+      <Route path='/' element={<Navbar />}>
+        <Route index path='home' element={<Home />} />
+        <Route path='shop' element={<Shop />}/>
+      </Route>
+    </Routes>  
   ); 
 }
 
