b7a70924c552cf1d442c1e96d56be6bec30d278a
Sergio Esteban Torres <myplancash@gmail.com>
2023-05-08 12:22:18 -0500

    created UserContext to wrap our App component

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from 'react'
+
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null
+})
+
+export const UserProvider = ({children}) => {
+  const [currentUser, setCurrentUser] = useState(null)
+  const user = {currentUser, setCurrentUser};
+
+  <UserContext.Provider user={user}>
+    {children}
+  </UserContext.Provider>
+}
