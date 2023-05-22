60ece06ffab5191e7c235962dce4ddbe163e88bf
Sergio Esteban Torres <myplancash@gmail.com>
2023-05-22 16:29:06 -0500

    added redirects file for netlify

--- src/components/Directory/Directory.styles.jsx
+++ src/components/Directory/Directory.styles.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const DirectoryContainer = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
