3db9e7f61d29682f2a902c4a986dbfc1b3abff6b
Sergio Esteban Torres <myplancash@gmail.com>
2023-05-24 12:08:10 -0500

    CREATING NEW user Reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -10,6 +10,7 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
